1b590e473d152e6b05cd5a742abb9f357f10aa40
Mathieu BREMOND <mathieu.bremond@ocelo.fr>
2020-05-16 23:34:07 +0700

    delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -189,4 +189,92 @@ router.delete('/likes/:id', auth, async (req, res) => {
     }
 });
 
+// @route   POST api/posts
+// @desc    comment on a post
+// @access  private
+router.post(
+    '/comment/:id',
+    [auth, [check('text', 'Text is required').not().isEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { text } = req.body;
+        const userId = req.user.id;
+        const id = req.params.id;
+
+        try {
+            const user = await User.findById(userId).select('-password');
+            if (!user)
+                return res
+                    .status(404)
+                    .json({ errors: [{ msg: 'User not found' }] });
+
+            const post = await Post.findById(id);
+            if (!post)
+                return res
+                    .status(404)
+                    .json({ errors: [{ msg: 'Post not found' }] });
+
+            const newComment = {
+                text,
+                user: userId,
+                name: user.name,
+                avatar: user.avatar,
+            };
+            post.comments.unshift(newComment);
+            await post.save();
+            return res.json(post.comments);
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).send('Internal server error');
+        }
+    }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    delete a comment on aa post
+// @access  private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    const id = req.params.id;
+    const commentId = req.params.comment_id;
+
+    try {
+        const post = await Post.findById(id);
+        if (!post)
+            return res
+                .status(404)
+                .json({ errors: [{ msg: 'Post not found' }] });
+
+        const comment = post.comments.find(
+            comment =>
+                comment._id.toString() === commentId &&
+                comment.user.toString() === req.user.id
+        );
+
+        if (!comment) {
+            return res
+                .status(404)
+                .json({ errors: [{ msg: 'Comment not found' }] });
+        }
+
+        const removeIndex = post.comments
+            .map(item => item._id.toString())
+            .indexOf(commentId);
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        return res.json(post.comments);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId')
+            return res.status(404).json({
+                errors: [{ msg: 'Post not found' }],
+            });
+        return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
